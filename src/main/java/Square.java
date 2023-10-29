0e143dc4a09522b7bba191e6731bc8a9ea432fc2
PavelM <pmartynovm@gmail.com>
2023-10-29 19:18:49 +0300

    new

---
+++ src/main/java/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args) {
+        int result, side = 12;
+        result = side * side;
+
+        System.out.println("Площадь квадрата равна: " + result);
+    }
+}
--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle {
+    public static void main(String [] args) {
+        int sideA = 12, sideB = 3;
+        double sinAB = 0.3;
+        double result = 0.5 * sideA * sideB * sinAB;
+
+        System.out.println("Площадь треугольника равна: " + result);
+    }
 }
